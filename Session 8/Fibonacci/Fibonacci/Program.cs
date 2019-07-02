460c7ed3977629b18da09263104cd0bfd2859f00
Joud Khattab <khattab.joud@gmail.com>
2019-07-03 01:54:40 +0300

    Add files via upload

---
+++ Session 8/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        // Recursion Solution (Exponential Complexity)
+        static int fib1(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return fib1(n - 2) + fib1(n - 1);
+            }
+        }
+
+        // Dynamic Solution "Top Down" (Linear Complexity)
+        static long[] visited = new long[100000];
+
+        static long fib2(int n)
+        {
+            if (visited[n]!=-1)
+            {
+                return visited[n];
+            }
+
+
+            if (n == 0 || n == 1)
+            {
+                visited[n] = n;
+                return n;
+            }
+            else
+            {
+                long res = fib2(n - 2) + fib2(n - 1);
+                visited[n] = res;
+                return res;
+            }
+        }
+
+        // Dynamic Solution "Bottom Up" (Linear Complexity)
+        static long fib3(int n)
+        {
+            long[] arr = new long[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for(int i=2;i<n+1;i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n];
+        }
+
+        static long fib4(int n)
+        {
+            long x1 = 0;
+            long x2 = 1;
+            long x3 = 0;
+            while (n>=2)
+            {
+                x3 = x2 + x1;
+                x1 = x2;
+                x2 = x3;
+                n--;
+            }
+            return x3;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000;i++ )
+            {
+                visited[i] = -1;
+            }
+
+            int num = 20;
+            Console.WriteLine("Dynamic Solution 'Bottom Up' (Linear Complexity): " + fib3(num));
+            Console.WriteLine("Dynamic Solution 'Top Down' (Linear Complexity): " + fib2(num));
+            Console.WriteLine("Recursion Solution (Exponential Complexity): " + fib1(num));
+            Console.WriteLine("-------------------------------------------------");
+
+            num = 40;
+            Console.WriteLine("Dynamic Solution 'Bottom Up' (Linear Complexity): " + fib3(num));
+            Console.WriteLine("Dynamic Solution 'Top Down' (Linear Complexity): " + fib2(num));
+            Console.WriteLine("Recursion Solution (Exponential Complexity): " + fib1(num));
+            Console.WriteLine("-------------------------------------------------");
+        }
+    }
+}
---
+++ Session 9/MAZE/MAZE/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAZE
+{
+    class Program
+    {
+        // find the number of roads to get out the maze
+        static void Main(string[] args)
+        {
+            // number of rows and columns
+            const int n=5;
+            const int m=5;
+
+
+            // declare the 2D array and fill it with zero's
+            int[,] maze = new int[n, m];
+
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    maze[i, j] = 0;
+                }
+            }
+
+            // and some walls to the maze
+            maze[0, 3] = -1;
+            maze[2, 1] = -1;
+
+
+            // print the array
+            //////////////////////////////////////////////////
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(maze[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------");
+            //////////////////////////////////////////////////
+            //////////////////////////////////////////////////
+
+            // fill the first column
+            for (int i=0;i<n;i++)
+            {
+                if(maze[i,0]==-1)
+                {
+                    break;
+                }
+                maze[i,0] = 1;
+            }
+
+            // fill the first row
+            for (int i = 0; i < m; i++)
+            {
+                if (maze[0, i] == -1)
+                {
+                    break;
+                }
+                maze[0, i] = 1;
+            }
+
+            // fill the array
+            for(int i=1;i<n;i++)
+            {
+                for(int j=1;j<m;j++)
+                {
+                    if(maze[i, j]==-1)
+                    {
+                        continue;
+                    }
+                    int x = maze[i - 1, j];
+                    int y = maze[i, j - 1];
+                    if(x!=-1)
+                    {
+                        maze[i, j] += x;
+                    }
+                    if(y!=-1)
+                    {
+                        maze[i, j] += y;
+                    }
+                }
+            }
+
+            // print the array
+            //////////////////////////////////////////////////
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(maze[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+            //////////////////////////////////////////////////
+            //////////////////////////////////////////////////
+        }
+    }
+}
